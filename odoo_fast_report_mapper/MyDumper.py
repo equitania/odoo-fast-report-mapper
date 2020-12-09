91bfb842502aa74f0db3d9193632f3f6ef18dde5
David Postolov <d.postolov@solisa.solutions>
2020-12-09 16:30:05 +0100

    [ADD] - Added MyDumper to structure yaml file

---
+++ odoo_fast_report_mapper/MyDumper.py
@@ -0,0 +1,7 @@
+import yaml
+
+
+# yaml.Dumper class to indent content correctly
+class MyDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(MyDumper, self).increase_indent(flow, False)
