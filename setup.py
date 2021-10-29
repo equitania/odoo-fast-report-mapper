08b3fe769c48e3112aa02219fbf8948edb2e2e24
Angelina Anakievska <a.anakievska@solisa.solutions>
2021-10-29 13:11:41 +0200

    [FIX] _search_report function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="odoo-fast-report-mapper-equitania",
-    version="0.1.0",
+    version="0.1.1",
     author="Equitania Software GmbH",
     author_email="info@equitania.de",
     description="A package to create FastReport entries in Odoo",
